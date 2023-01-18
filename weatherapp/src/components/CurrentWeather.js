2133690d6757a8ba4cfea8b490d7e4c63e9b5d4e
Jimmy <jlam31@ucsc.edu>
2023-01-18 13:50:15 -0800

    connected weather api to app

--- weatherapp/src/components/CurrentWeather.js
+++ weatherapp/src/components/CurrentWeather.js
@@ -4,18 +4,42 @@ import testLogo from '../weather/64x64/day/113.png'
 
 
 import { Container, flexbox } from '@mui/system';
+import { useEffect, useState } from 'react';
 
 
 const CurrentWeather = () => {
+
+    const [location, setLocation] = useState('');
+    const [currCondition, setCurrCondition] = useState('');
+    const [currTemp, setCurrTemp] = useState('');
+
+    useEffect( () => {
+        fetch('http://api.weatherapi.com/v1/current.json?key=4a24417b627b4e3d9ca232624231301&q=auto:ip')
+            .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch data');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setLocation(data.location.name);
+                setCurrCondition(data.current.condition.text);
+                setCurrTemp(data.current.temp_f);
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    })
+
     return ( 
         <Box sx={{width:1}}>
             <Container sx={{ border:1}}>
-                <h2>Portland, Oregon</h2>
-                <p>Wednesday, 10:48 AM, Scattered Showers</p>
+                <h2>{location}</h2>
+                <p>{currCondition}</p>
 
                 <Box sx={{ textAlign: 'left', mt:4, display:'grid' }}>
                     <Box sx={{ gridRow:'1'}}>
-                        <h2 style={{display:"flex", justifyContent:"center"}}>20&#176;F</h2>
+                        <h2 style={{display:"flex", justifyContent:"center"}}>{currTemp}&#176;F</h2>
                        
                     </Box>
                     <Box sx={{ gridRow:'1', height:300, bgcolor:'success.main'}}>
