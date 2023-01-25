ca219465abb0fdc52f1e815b0e8ed8cad399a88c
Jimmy <jlam31@ucsc.edu>
2023-01-25 13:11:58 -0800

    fixed issue with conditional rendering

--- weatherapp/src/components/WeeklyForecast.js
+++ weatherapp/src/components/WeeklyForecast.js
@@ -3,12 +3,15 @@ import ForecastBar from "./ForecastBar";
 
 const WeeklyForecast = () => {
 
+    /*
     const [date, setDate] = useState([]);
     const [minTemp, setMinTemp] = useState([]);
     const [maxTemp, setMaxTemp] = useState([]);
     const [condition, setCondition] = useState([]);
+    */
 
     const [forecasts, setForecasts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect( () => {
         fetch(`http://api.weatherapi.com/v1/forecast.json?key=4a24417b627b4e3d9ca232624231301&q=auto:ip&days=3`)
@@ -20,6 +23,7 @@ const WeeklyForecast = () => {
             })
             .then(data => {
                 setForecasts(data.forecast.forecastday);
+                setLoading(false);
                
             })
             .catch(err => {
@@ -31,9 +35,9 @@ const WeeklyForecast = () => {
 
     return ( 
         <div>
-            <ForecastBar date={forecasts[0].date} maxTemp={forecasts[0].day.maxtemp_f} minTemp={forecasts[0].day.mintemp_f} condition={forecasts[0].day.condition.text} />
-            <ForecastBar date={forecasts[1].date} maxTemp={forecasts[1].day.maxtemp_f} minTemp={forecasts[1].day.mintemp_f} condition={forecasts[1].day.condition.text} />
-            <ForecastBar date={forecasts[2].date} maxTemp={forecasts[2].day.maxtemp_f} minTemp={forecasts[2].day.mintemp_f} condition={forecasts[2].day.condition.text} />
+            { !loading && <ForecastBar date={forecasts[0].date} maxTemp={forecasts[0].day.maxtemp_f} minTemp={forecasts[0].day.mintemp_f} condition={forecasts[0].day.condition.text} /> }
+            { !loading && <ForecastBar date={forecasts[1].date} maxTemp={forecasts[1].day.maxtemp_f} minTemp={forecasts[1].day.mintemp_f} condition={forecasts[1].day.condition.text} /> }
+            { !loading && <ForecastBar date={forecasts[2].date} maxTemp={forecasts[2].day.maxtemp_f} minTemp={forecasts[2].day.mintemp_f} condition={forecasts[2].day.condition.text} /> }
         </div>
      );
 }
